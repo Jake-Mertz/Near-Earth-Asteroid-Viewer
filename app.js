218bef18a80d36a5f4ecb6c7bbf0b24b162e7049
Jake Mertz <jakemertz101@gmail.com>
2020-08-04 21:00:24 -0400

    pre re organization

---
+++ app.js
@@ -0,0 +1,28 @@
+class APP {
+  // getArray() {
+  //   var formData = new FormData(event.target);
+  //   var name = formData.get('name');
+  //   var diameterMin = formData.get('estimated_diameter.kilometers.estimated_diameter_min');
+  //   var diameterMax = formData.get('estimated_diameter.kilometers.estimated_diameter_max');
+  //   var velocity = formData.get('close_approach_data.0.relative_velocity.kilometers_per_hour')
+  //   var firstObservation = formData.get('orbital_data.first_observation_date');
+  //   var lastObservation = formData.get('orbital_data.last_observation_date');
+  //   var missDistance = formData.get('close_approach_data.0.miss_distance.kilometers')
+  //   var isHazardous = formData.get('is_potentially_hazardous_asteroid');
+  // }
+  renderTable() {
+    var newRow = document.createElement('tr');
+    var newTd1 = document.createElement('td');
+    var newTd2 = document.createElement('td');
+    var newTd3 = document.createElement('td');
+    var newTd4 = document.createElement('td');
+    var newTd5 = document.createElement('td');
+    var newTd6 = document.createElement('td');
+    var newTd7 = document.createElement('td');
+    var newTd8 = document.createElement('td');
+    var newTd9 = document.createElement('td');
+    var newTd10 = document.createElement('td');
+
+  }
+
+}
